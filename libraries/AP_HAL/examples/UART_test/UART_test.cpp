c68cdd215ecec8e0cc549678ba02899d9a4825f4
Asa Davis <asadavis1002@gmail.com>
2021-03-24 01:06:45 -0600

    add parsing support to test sketch

--- libraries/AP_HAL/examples/UART_test/UART_test.cpp
+++ libraries/AP_HAL/examples/UART_test/UART_test.cpp
@@ -19,6 +19,7 @@ static void buildPacket();
 static bool validPacket();
 static void printCurrPacket();
 //static void printAllAvailableBytes();
+static void accelGyroData(uint8_t * fieldData, float arr[]);
 
 //HAL variables
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
@@ -45,7 +46,7 @@ static uint32_t firstReadTime;
 static int numGoodPackets = 0;
 static int numBytesSkipped = 0;
 static int numBadPackets = 0;
-static bool debugged = false;
+//static bool debugged = false;
 
 void setup() {
     setup_uart(imu);
@@ -55,19 +56,19 @@ void setup() {
 
 void loop() {
     //read 1000 packets, then debug the stats
-    if(numGoodPackets < 1000) {
+    //if(numGoodPackets < 1000) {
         //collect all available bytes
         readIMU();
 
         //parse any available bytes
         buildPacket();
-    }
-    else if(!debugged) {
-        console->printf("\nread 1000 packets in %lu milliseconds\n", AP_HAL::millis() - firstReadTime);
-        console->printf("skipped %i bytes\n", numBytesSkipped);
-        console->printf("handled %i bad checksums\n", numBadPackets);
-        debugged = true;
-    }
+    //}
+    //else if(!debugged) {
+        //console->printf("\nread 1000 packets in %lu milliseconds\n", AP_HAL::millis() - firstReadTime);
+        //console->printf("skipped %i bytes\n", numBytesSkipped);
+        //console->printf("handled %i bad checksums\n", numBadPackets);
+        //debugged = true;
+    //}
 }
 
 //read all available bytes into ring buffer.
@@ -97,7 +98,7 @@ static void buildPacket() {
                     numBytesSkipped++;
                     nextSyncByte = syncByte1;
                 }
-                }
+            }
                 break;
             case payloadSize: {
                 buffer.peekbytes(tempData, searchBytes);
@@ -105,7 +106,7 @@ static void buildPacket() {
                 currPacket.header[3] = tempData[1];
                 searchBytes = tempData[1] + 4; //next time we need to peek the second half of the header (which we already peeked) + payload + checksum
                 currPhase = payloadAndChecksum;
-                }
+            }
                 break;
             case payloadAndChecksum: {
                 buffer.peekbytes(tempData, searchBytes);
@@ -128,7 +129,7 @@ static void buildPacket() {
                 }
                 currPhase = sync;
                 searchBytes = 1;
-                }
+            }
                 break;
         }
     }
@@ -152,24 +153,68 @@ static bool validPacket() {
     return (currPacket.checksum[0] == checksumByte1 && currPacket.checksum[1] == checksumByte2);
 }
 
+
+void accelGyroData(uint8_t * fieldData, float arr[]) {
+    //vector<float> xyzData;
+    uint32_t midx = 0;
+    for (uint8_t i = 0; i < 4; i++ ) {
+        midx = (midx << 8) + static_cast<uint32_t>(fieldData[i]);
+    }
+    uint32_t midy = 0;
+    for (uint8_t i = 4; i < 8; i++ ) {
+        midy = (midy << 8) + static_cast<uint32_t>(fieldData[i]);
+    }
+    uint32_t midz = 0;
+    for (uint8_t i = 8; i < 12; i++ ) {
+        midz = (midz << 8) + static_cast<uint32_t>(fieldData[i]);
+    }
+    arr[0] = ( (* (float *) &midx) ); // Reinterpret_cast does not work for this so
+    arr[1] = ( (* (float *) &midy) );
+    arr[2] = ( (* (float *) &midz) );
+    //xyzData.push_back(* reinterpret_cast<float*>(&midx)); // Works, but its doing the same thing as above, if you do it without address' it doesn't work
+    //return xyzData;
+}
+
 static void printCurrPacket() {
-    for(int i = 0; i < 4; i++) {
+    /*for(int i = 0; i < 4; i++) {
         console->printf("0x%x ", currPacket.header[i]);
     }
     for(int i = 0; i < currPacket.header[3]; i++) {
         console->printf("0x%x ", currPacket.payload[i]);
     }
     for(int i = 0; i < 2; i++) {
-        console->printf("0x%x ", currPacket.checksum[i]);
+        console->printf("0x%x ", currPacket.checksum[i]); 75 65 80 A3
     }
-    console -> printf("\n");
+    console -> printf("\n");*/
+    uint8_t payloadLength = currPacket.header[3];
+// length descriptor, 12bit payload
+    for (uint8_t i = 0; i < payloadLength; i += currPacket.payload[i]) {
+        //uint8_t fieldLength = currPacket.payload[i]; //Each field in the payload has its own length property as the first byte of the field
+        uint8_t fieldDescriptor = currPacket.payload[i+1]; // Second byte is the field descriptor. Whether it is accel or gyro, etc.
+        //ctor<float> xyz; // Maybe call accelGyroData() here instead to reduce reuse of code but could be a waste if we include other data in the field like GPS data
+        float retarr[3];
+        accelGyroData(currPacket.payload + (i+2),retarr); // This will probably break if the packet is wrong
+        switch (fieldDescriptor) {  // Switch to relevant course for accel or gyro or etc. data
+            case 4:
+                //console->printf("Accel - X: %f,\tY: %f,\tZ: %f\n",retarr[0], retarr[1], retarr[2]);
+                break;
+            case 5:
+                console->printf("Gyro  - X: %f,\tY: %f,\tZ: %f\n",retarr[0], retarr[1], retarr[2]);
+                // cout << "Gyro:\nX: " << xyz[0] << "\nY: " << xyz[1] << "\nZ: " << xyz[2] << '\n';
+                // firstg = (firstg + 1) % BUFFSIZE; // Before we push on data increment the index so that firstg will equal the most recent data
+                // gyroBuffer[firstg] = xyz; // Push vector with gyro xyz onto the buffer
+                break;
+            default:
+                //cout << "Don't want anything but Gyro and Accel\n";
+                break;
+        }
+    }
 }
 
 //just prints whatever is in the ring buffer
 /*static void printAllAvailableBytes() {
     uint32_t available = buffer.available();
     buffer.read(tempData, available);
-
     for(int i = 0; i < available; i++) {
         console -> printf("0x%x ", tempData[i]);
     }
